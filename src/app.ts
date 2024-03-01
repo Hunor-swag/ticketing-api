d4278202ea84bc0facc557d5b9595acb0954eaa2
Hunor <mogyorosi.hunor@gmail.com>
2024-03-01 15:35:07 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,24 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import customerRoutes from './routes/customerRoutes';
-import authRoutes from './routes/authRoutes';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import customerRoutes from "./routes/customerRoutes";
+import authRoutes from "./routes/authRoutes";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
+
 // app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/customers', customerRoutes);
+app.use("/customers", customerRoutes);
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
