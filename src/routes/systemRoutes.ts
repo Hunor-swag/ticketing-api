c26227ad1d37feb37083482bb8d6e64ab20ca548
Hunor-swag <mogyorosi.hunor@gmail.com>
2024-04-10 13:45:17 +0200

    update

--- src/routes/systemRoutes.ts
+++ src/routes/systemRoutes.ts
@@ -4,6 +4,7 @@ import systemController from '../controllers/systemController';
 const systemRoutes = express.Router();
 
 systemRoutes.post('/create', systemController.createSystem);
+systemRoutes.get('/get-all', systemController.getSystems);
 systemRoutes.post('/update-all', systemController.updateAllSystems);
 
 export default systemRoutes;
